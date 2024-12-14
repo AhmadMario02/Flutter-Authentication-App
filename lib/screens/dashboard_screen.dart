f13a93b9daf8ca30b15c520ca65cd36ed5261d3a
Ahmad Mario <riotheassassin@gmail.com>
2024-12-14 07:37:36 +0700

    Adding menu drawer

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'login_screen.dart';
+import 'form_mahasiswa.dart';
+import 'list_mahasiswa.dart';
 
 class DashboardScreen extends StatelessWidget {
   final User user;
@@ -9,7 +11,17 @@ class DashboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Dashboard'),
+        title: const Text('Dashboard'),
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+        ),
         actions: [
           IconButton(
             icon: Icon(Icons.logout),
@@ -21,6 +33,38 @@ class DashboardScreen extends StatelessWidget {
             },
           ),
         ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.list_alt),
+              title: const Text('Form Mahasiswa'),
+              onTap: () => Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => FormMahasiswa()))
+            ),
+            ListTile(
+              leading: const Icon(Icons.list),
+              title: const Text('List Mahasiswa'),
+              onTap: () => Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ListMahasiswaPage()))
+            ),
+          ],
+        ),
       ),
       body: Center(
         child: Column(
